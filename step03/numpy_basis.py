d37dd543d4e42311462bf189d30533fbca327af4
ToB213 <niimicomputergraphics@gmail.com>
2026-03-25 20:18:29 +0900

    ✨ 多次元正規分布の描画

---
+++ step03/numpy_basis.py
@@ -0,0 +1,33 @@
+import numpy as np
+
+x = np.array([1, 2, 3])
+
+print(x.__class__)
+print(x.shape)
+print(x.ndim)
+
+W = np.array([[1, 2, 3],
+              [4, 5, 6]])
+print(W.shape)
+print(W.ndim)
+
+X = np.array([[0, 1, 2],
+              [3, 4, 5]])
+print(W+X)
+print('---')
+print(W*X)
+
+# ベクトルの内積
+a = np.array([1, 2, 3])
+b = np.array([4, 5, 6])
+y = np.dot(a, b)
+print(y)
+
+# 行列の積
+A = np.array([[1, 2], [3, 4]])
+B = np.array([[5, 6], [7, 8]])
+Y = np.dot(A, B)
+print(Y)
+
+# ベクトルの内積と行列積の計算には@という演算子を利用できる
+# a @ b = np.dot(a, b)
